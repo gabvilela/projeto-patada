bf770b1a3261530dc4c896affc432e594fcb9e69
Gabriela Vilela <gabbrielavilela@gmail.com>
2024-07-31 09:56:10 +0100

    Ajustes na página de adoção, inserção do toast no contacto e ajuste no arquivo js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,27 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-// Close mobile naviagation
-if (link.classList.contains("main-nav-link"))
-  headerEl.classList.toggle("nav-open");
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+//////////////////////// INTERACTION
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede a submissão real do formulário
+
+    // Mostra o toast
+    var toastElement = document.getElementById("successToast");
+    var toast = new bootstrap.Toast(toastElement);
+    toast.show();
+
+    this.reset();
+  });
